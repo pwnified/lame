7229f5f9cf776e85581cdb1a6db7a403ad105a24
Hamilton <pwnified@harmonicdog.com>
2024-04-12 20:00:15 -0700

    XCFramework

---
+++ macosx/LameXC/LameXC.h
@@ -0,0 +1,18 @@
+//
+//  LameXC.h
+//  LameXC
+//
+//  Created by Hami on 4/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LameXC.
+FOUNDATION_EXPORT double LameXCVersionNumber;
+
+//! Project version string for LameXC.
+FOUNDATION_EXPORT const unsigned char LameXCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LameXC/PublicHeader.h>
+//#import <../include/lame.h>
+
